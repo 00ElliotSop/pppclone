16f1b375b48884a14d502fdb2ed6f22944d13761
Elliot:Sop:007 <elliotsop007@gmail.com>
2025-10-22 21:43:58 -0400

    Adjust margins and image display properties

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -147,14 +147,14 @@ const Home = () => {
 
   
   
-<section className="relative mt-[1.3rem] lg:mt-0 lg:h-screen bg-white">
+<section className="relative mt-[.9rem] lg:mt-0 lg:h-screen bg-white">
         {/* Preload critical images */}
         <link rel="preload" as="image" href={heroImages[0]} fetchpriority="high" />
         <link rel="preload" as="image" href={heroImages[1]} fetchpriority="high" />
         <link rel="preload" as="image" href={heroImages[2]} fetchpriority="high" />
         <link rel="preload" as="image" href={heroImages[3]} fetchpriority="high" />
         <link rel="preload" as="image" href="/Meetdateam.gif" fetchpriority="high" />
-<div className="relative w-full h-[62vh] lg:h-full mt-[1.47vh]">
+<div className="relative w-full h-[47vh] lg:h-full mt-[1.47vh]">
           {heroImages.map((image, index) => (
             <div
               key={index}
@@ -166,7 +166,7 @@ const Home = () => {
       <img
   src={image}
   alt={`Hero ${index + 1}`}
-  className="absolute inset-0 w-full h-full object-cover object-center transition-all duration-700 ease-in-out"
+  className="absolute inset-0 w-[90%] h-[80%] object-contain object-center transition-all duration-700 ease-in-out"
   loading="eager"
   decoding="sync"
 />
@@ -286,7 +286,7 @@ const Home = () => {
 </div>
 
   
-<div className="mt-12 lg:mt-9"></div>
+<div className="mt-12 lg:mt-4"></div>
 
 
     
